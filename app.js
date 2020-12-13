19255bb4e52259ae2533da3d4577623ee05f3712
Marleyman876 <grant_garfield@yahoo.com>
2020-12-12 23:56:56 -0800

    made interative message

---
+++ app.js
@@ -0,0 +1,15 @@
+console.log("Hello, my name is Garfield.");
+
+var promptQuestion = "Will Garfield become a good developer?"
+var response = prompt (promptQuestion); 
+console. log(response);
+
+var alertMessage: 
+
+if (response === "No") {
+    alertMessage = "No one really knows, we shall see"
+} else if (response== "Yes") {
+    alertMessage = "Yea your right he's a pretty driven and motivated guy!"
+} else {
+    alertMessage = "I think he will do great things."
+}
