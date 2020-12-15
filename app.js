7de5554e7832aa5b5e51c16ff14bfe7d13fc5f6e
Marleyman876 <grant_garfield@yahoo.com>
2020-12-14 22:48:44 -0800

    created first propmt function

--- app.js
+++ app.js
@@ -1,54 +1,87 @@
-console.log("Hello, my name is Garfield.");
+var response;
 
-var promptQuestion = "Will Garfield become a good developer?";
-var response = prompt(promptQuestion); 
-console. log(response);
-
-var alertMessage;
-
-if (response == "No") {
-    alertMessage = "No one really knows, we shall see";
-} else if (response == "Yes") {
-    alertMessage = "Yea your right he's a pretty driven and motivated guy!";
-} else {
-    alertMessage = "I think he will do great things.";
+function getPrompt() {
+    var promptQuestion = "Will Garfield become a good developer?";
+    response = prompt(promptQuestion); //value of whatever user gives us
 }
-alert(alertMessage);
 
-var promptQuestion = "Do you like ice cream?";
-var response = prompt(promptQuestion); 
-console.log (response);
+getPrompt();
 
-if (response == "yes"){
-    alertMessage = "good choice I like Ice Cream also";
-} else if (response = "no"){
-    alertMessage = "why not? ice cream is good for you";
+var color;
+
+function colorResponse(rsp) {
+    if (rsp === "Yes"){
+        color = "lightgreen";
+    } else if (rsp === "No"){
+        color = "black";
+    }
 }
-alert (alertMessage);
 
-var promptQuestion = "Do you like Beer?";
-var response = prompt(promptQuestion); 
-console.log (response);
+colorResponse(response);
 
-if (response == "yes"){
-    alertMessage = "good choice beer is delicious";
-} else if (response = "no"){
-    alertMessage = "wise man alcohol isnt good for you";
-}
-alert (alertMessage);
 
-// function begin (string1, string2) {
-//     let bigstring = string1+" "+string2
-//     alert(bigstring)
+document.getElementById("head").style.backgroundColor = color
+//document.body.style.backgroundColor = "lightgreen"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// if (response == "No") {
+//     alertMessage = "No one really knows, we shall see";
+// } else if (response == "Yes") {
+//     alertMessage = "Yea your right he's a pretty driven and motivated guy!";
+// } else {
+//     alertMessage = "I think he will do great things.";
 // }
-// begin("start", "end")
+// alert(alertMessage);
+
+// var promptQuestion = "Do you like ice cream?";
+// var response = prompt(promptQuestion); 
+// console.log (response);
+
+// if (response == "yes"){
+//     alertMessage = "good choice I like Ice Cream also";
+// } else if (response = "no"){
+//     alertMessage = "why not? ice cream is good for you";
+// }
+// alert (alertMessage);
+
+// var promptQuestion = "Do you like Beer?";
+// var response = prompt(promptQuestion); 
+// console.log (response);
+
+// if (response == "yes"){
+//     alertMessage = "good choice beer is delicious";
+// } else if (response = "no"){
+//     alertMessage = "wise man alcohol isnt good for you";
+// }
+// alert (alertMessage);
+// // function begin (string1, string2) {
+// //     let bigstring = string1+" "+string2
+// //     alert(bigstring)
+// // }
+// // begin("start", "end")
 
 // function addThree (a, b, c){ //declaring a function
-//     let answer = a+b+c  //declaring the variable answer and assigning it to the result of a+b+c
-//     alert(answer) //calling a function alert, passing answer as the parameter
+// //     let answer = a+b+c  //dec e variable answer and assigning it to the result of a+b+c
+// //     alert(answer) //calling a function alert, passing answer as the parameter
 // }
 
-// addThree (22,33,44) 
+// // addThree (22,33,44) 
 
 // function addThree (a, b, c){ //declaring a function called addThree with  3 params
 //     let answer = a+b+c  //declaring the variable answer and assigning it to the result of a+b+c
@@ -56,4 +89,4 @@ alert (alertMessage);
 //     console.log ("whats for dinner")  //will never get called
 // }
 // let sum = addThree (22,33,44) //declare a var called sum
-// alert (sum)  //calling function alert with param sum in order to get its side effect. a side effect is something a function does that is not related to its return. 
+// alert (sum)  //calling function alert with param sum in order to get its side effect. a side effect is something a function does that is not related to its return.
